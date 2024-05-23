18bd8eec2d9d466bbb7d949b527e6fd07feef198
Seolyoona <144825458+foryoonas@users.noreply.github.com>
2024-05-23 18:09:17 +0900

    Add files via upload

---
+++ 1week/ex2.py
@@ -0,0 +1,12 @@
+text="설윤아"
+print("이름:"+text)
+
+text1="202310467"
+text2="2학년"
+print("학번 및 학년:"+text1+"/"+text2)
+
+text3="드라마보기 or 좋아하는 사람 영상보기"
+print("취미:"+text3)
+
+text4="건물주"
+print("진로:"+text4)
