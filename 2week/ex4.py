a74a5b2a175b828c114bb49733e3bee07795bfe6
Seolyoona <144825458+foryoonas@users.noreply.github.com>
2024-05-25 04:49:20 +0900

    Add files via upload

---
+++ 2week/ex4.py
@@ -0,0 +1,18 @@
+'''방법1'''
+print("한 개의 정수를 입력하세요.")
+num1=int(input())
+if num1 % 2 == 0 :
+    print("짝수이다-True")
+if num1 % 2 == 1:
+    print("홀수이다-False")
+
+'''방법2'''
+a=int(input())
+b= a%2
+c=b==0
+print(c)
+
+
+
+
+
