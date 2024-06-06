f9e6660f699140e6adec7d28c76947f982a4344e
Seolyoona <144825458+foryoonas@users.noreply.github.com>
2024-06-07 03:23:26 +0900

    Add files via upload

---
+++ 11week/ex2.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+data = {'이름':['Kim','Park','Lee','Ho'],
+        '국어' :[90,58,88,100],
+        '영어' :[100,60,80,70],
+        '수학' :[55,65,76,88]}
+
+df=pd.DataFrame(data)
+
+print(df,end='\n\n')
+
+sr_name=df['이름']
+print(sr_name,end='\n\n')
+
+park_data=df.loc[1]
+print(park_data, end='\n\n')
+park_data=df.loc[df['이름']=='park']
+print(park_data,end='\n\n')
+
+df.loc[df['이름']=='Ho','수학']=90
+print(df,end='\n\n')
+
+df.loc[5]=['Oh',100,70,80]
+print(df,end='\n\n')
+
+df=df.drop([2],axis=0)
+print(df,end='\n\n')
