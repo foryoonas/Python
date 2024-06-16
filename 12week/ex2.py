c67588347b771cc6c77e38cc14d94b11f28951d3
Seolyoona <144825458+foryoonas@users.noreply.github.com>
2024-06-17 03:15:47 +0900

    Add files via upload

---
+++ 12week/ex2.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import mariadb
+
+conn = mariadb.connect(
+    user='user014',
+    password='!ai123',
+    host='edu.ithows.com',
+    port=53306,
+    database='edudb'
+)
+
+cursor = conn.cursor()
+
+cursor.execute("SELECT * FROM city")
+results = cursor.fetchall()
+for row in results:
+    print(row)
+    
+df = pd.DataFrame(results)
+print(df.head(10), end='\n\n')
+    
+conn.close()
